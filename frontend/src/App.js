a91cc5164a4c52899432f6bce49680abf4fb56da
obapluto-ob <obed.lopeyok@student.moringaschool.com>
2025-11-07 13:14:53 -0800

    Switch to clean login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import Login from './pages/Login';
+import Login from './pages/NewLogin';
 import Dashboard from './pages/Dashboard';
 import Wallets from './pages/Wallets';
 import Transactions from './pages/Transactions';
